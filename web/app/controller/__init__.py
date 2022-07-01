7b8ee0c69a13a4a8b297eabb2e350f60679ad22b
Sanket <sanketkalode123@gmail.com>
2022-07-01 22:23:32 +0530

    api version update

--- web/app/controller/__init__.py
+++ web/app/controller/__init__.py
@@ -6,5 +6,5 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return jsonify({
-        'message': "Welcome to book.com"
+        'message': "Welcome to book.com v.2"
     }), 200
